7e8ea2c5bf2fffca22b26c5c47f6bd059ebc4654
GitHub-Account-Saad <159918553+SaadTahir1@users.noreply.github.com>
2024-03-28 02:44:39 +0500

    Add files via upload

---
+++ project01_number_guessing_game/index.js
@@ -0,0 +1,15 @@
+console.log(" Guess a number between 1 to 8 ");
+let x = Math.floor(Math.random() * 7);
+import inquirer from "inquirer";
+while (true) {
+    let input = await inquirer.prompt({ name: "guess", type: "number",
+        message: "Enter your guess number you want between 1 to 8: " });
+    let ans = input.guess;
+    if (ans == x) {
+        console.log("Congratulations! your guess is right");
+        break;
+    }
+    else {
+        console.log("your guess is wrong try again!!");
+    }
+}
